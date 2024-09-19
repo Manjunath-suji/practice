f8dfb3329a1cdb712c2f45fcdaab8d42e0c5d46f
Manjunath-suji <manjunathkp2004@gmail.com>
2024-09-19 12:06:35 +0530

    adding

--- binary string/Main.java
+++ binary string/Main.java
@@ -1,6 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    //to convert binary string to alphabet
     // 111011 ->CB
     public static void converter(String n){
         String ans="";
@@ -22,7 +23,8 @@ public class Main {
         }
         System.out.println(ans);
     }
-
+//-------------------------------------------------------------------------------------------------
+//to capitalize the string
     public  static String cap(String s){
        //sDFGYhjb -> Sdfgyhjb
         s=s.toLowerCase();
@@ -34,6 +36,34 @@ public class Main {
         return h;
 
     }
+    //---------------------------------------------------------------------------------------------
+    //to see whether string is valid or not
+    public static int valid(String s,int g){
+
+        int countn=0,counta=0;
+        char[] t=s.toCharArray();
+        if(g>4 && !(Character.isDigit(s.charAt(0)))){
+            for(int i=0;i<t.length;i++){
+                if(Character.isUpperCase(t[i])){
+                    counta++;
+                }
+                if(Character.isDigit(t[i])){
+                    countn++;
+                }
+                if(t[i]=='/' || t[i]==' '){
+                    return 0;
+
+                }
+            }
+        }
+
+        if(counta>0 && countn>0){
+            return 1;
+        }
+
+        return 0;
+    }
+    //--------------------------------------------------------------------------------------------------------------------------------------
 
     public static void main(String[] args) {
 
